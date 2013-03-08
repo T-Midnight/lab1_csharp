832e2386b32d486fecd19a7051f55beb6622803e
T-Midnight <BelousovaTanya1995@yandex.ru>
2013-03-08 18:23:33 +0300

    first commit

---
+++ CsArray/CsArray/CsArray/CsArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;				
+using System.Text;
+
+namespace CsArray {
+	class Program {
+		static void Main(string[] args) 
+        {
+			int[] A = new int[5];
+            int min, max;
+           
+			for (int i = 0; i < 5; i++) {
+				Console.WriteLine("Введите элемент массива:", i);
+				A[i] = Convert.ToInt32(Console.ReadLine());	// если ввести буквы, то программа упадёт
+			}
+
+            try { int el = Int32.Parse(Console.ReadLine()); }
+            catch (FormatException) { 
+                Console.WriteLine("Некорректно. Повторите ввод."); 
+            }
+
+            min = A[0];
+			for (int i = 1; i < 5; i++) {
+				if (A[i] < min)
+					min = A[i];
+			}
+
+            max = A[0];
+			for (int i = 1; i < 5; i++) {
+				if (A[i] > max) 
+					max = A[i];
+			}
+
+			Console.WriteLine("Минимальный элемент = " + min);
+			Console.WriteLine("Максимальный элемент = " + max);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ CsArray/CsArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[5]; 
+            int Min = A[0]; int Max = A[0];
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine("Введите элемент массива:", i);
+                A[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+
+            for (int i = 1; i < 5; i++) {
+                if (A[i] < Min ) {
+                    Min = A[i];
+                }
+            }
+
+            for (int i = 1; i < 5; i++) { 
+                if ( A[i] > Max ) {  
+                    Max = A[i];       
+                }
+            }
+
+            Console.WriteLine("Минимальный элемент = " + Min);
+            Console.WriteLine("Максимальный элемент = " + Max);
+            Console.ReadKey();
+        }
+    }
+}
